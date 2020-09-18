56c85ecc95c583dd20c81e90c42fc2d5b01f5dfa
Ayushknp <70503583+Ayushknp@users.noreply.github.com>
2020-09-18 13:33:04 +0530

    Add files via upload

--- PRO_1.js
+++ PRO_1.js
@@ -1,15 +1,99 @@
-function OneRes()
+let targets=[{
+    name:'Shreyansh',
+    age :20,
+    city : 'delhi',
+    salary : 20000,
+},
 {
-    const element = document.getElementsByClassName("MSD");
-        element[0].src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTrMMaoFEEIYliVM7HPGsAPad8rN0H8LF0WZg&usqp=CAU";
+    name: 'sandeep',
+    age : 20,
+    city : 'lucknow',
+    salary : 50000,
+},
+{
+    name: 'david',
+    age : 20,
+    city : 'russia',
+    salary : 75000,
+},
+{
+    name: 'roman',
+    age : 20,
+    city : 'london',
+    salary : 60000,
+}];
+
+
+function display(targets){
+    let target_data='';
+    let c=1;
+   
+    targets.forEach(function(element,index){
+        let presetrow=`<tr>
+                <td>${c}</td>
+                <td>${element.name}</td>
+                <td>${element.age}</td>
+                <td>${element.city}</td>
+                <td>${element.salary}</td>
+                <td><button onclick="delete_ele(${index})" class="del_btn">delete</button></td>`;
+
+                
+
+
+        target_data+=presetrow;
+
+
+
+    c++;
+    });
+    document.getElementById('tablerows').innerHTML = target_data;  
+};
+
+function insert(e){
+    e.preventDefault()
+    let target={};
+    let ins_name=document.getElementById("ins-name").value;
+    let ins_age = document.getElementById("ins-age").value;
+    let ins_city = document.getElementById("ins-city").value;
+    let ins_salary= document.getElementById("ins-salary").value;
+    target.name=ins_name;
+    target.age = Number(ins_age);
+    target.city = ins_city;
+    target.salary =Number(ins_salary);
+    console.log(ins_salary);
+    
+    targets.push(target);
+    display(targets);
+return false;
 }
-function SecondRes()
+
+display(targets);
+
+function search()
 {
-    const element = document.getElementsByClassName("MSD");
-        element[0].src = "https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-b0t698fcfvti5jddor9hr4jju1-20200203160346.Medi.jpeg";
+  let ser_ele= document.getElementById("searching").value;
+ let ser_res_arr=targets.filter(function(targets){
+     if (targets.name.indexOf(ser_ele) != -1)
+     {
+       return targets.name.indexOf(ser_ele) != -1;
+     }
+     else if (targets.city.indexOf(ser_ele) != -1){
+         return targets.city.indexOf(ser_ele) != -1;
+           
+
+     }
+     ;
+
+ })
+
+ display(ser_res_arr);
+
 }
-function ThirdRes()
-{
-    const element = document.getElementsByClassName("MSD");
-        element[0].src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSYDhGYZyZ7EDkZb3SjQJ1jbKdCkPXitYE35I6xcjMttajJLRf703MoJC4A7kRNPhB9SykMbQ0Hhg9blYSFSxmuXPme46IO6DI7dA&usqp=CAU&ec=45699844";
+
+function delete_ele(index){
+    console.log(index);
+    targets.splice(index,1);
+    display(targets);
+    
+
 }
